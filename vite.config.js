ba6e95521610b3fa8358275a6f2a9e741b4159f7
Szymon Antczak <szymon0antczak@gmail.com>
2024-06-12 09:26:22 +0200

    Add multilanguage support

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,35 @@
 import { defineConfig } from "vite";
 import { ViteEjsPlugin } from "vite-plugin-ejs";
-import pl from './i18n/pl.json';
+import fs from "fs";
+import { resolve } from "path";
+
+const locales = ["pl", "en"];
+
+const loadLocaleData = (locale) => {
+  const filePath = resolve(__dirname, `src/i18n/${locale}.json`);
+  return JSON.parse(fs.readFileSync(filePath, "utf-8"));
+};
+
+const generateData = () =>
+  locales.reduce((acc, locale) => {
+    acc[locale] = loadLocaleData(locale);
+    return acc;
+  }, {});
 
 export default defineConfig({
-  base: './',
+  base: "./",
   root: "src",
   publicDir: "../public",
+  plugins: [
+    ViteEjsPlugin(generateData()),
+  ],
   build: {
     outDir: "../docs",
+    rollupOptions: {
+      input: {
+        pl: resolve(__dirname, 'src/index.html'),
+        en: resolve(__dirname, 'src/en/index.html'),
+      },
+    },
   },
-  plugins: [
-    ViteEjsPlugin({
-      ...pl,
-    }),
-  ],
 });
